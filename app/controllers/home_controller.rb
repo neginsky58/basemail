55170ceebef3b70f9f74415405c5afec70d8dcb4
root <neginsky58@gmail.com>
2014-02-28 03:49:12 +0200

    first

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,27 +1,49 @@
 class HomeController < ApplicationController
-
-  def index
-  end
-
+ 
   def new
     @draft = Draft.new
   end
 
-  def create    
-    
-    flash[:success] = t('email.success_sent')
-    redirect_to action: 'index'
+  def create
+    respond_to do |format|
+      # if params[:commit] == 'Send'
+      #   if is_a_valid_email(params[:to])
+      #     ActionMailer::Base.mail(:from => "yevgeny@example.com", :to => params[:to], :subject => params[:subject], :body => params[:body]).deliver      
+      #     flash[:success] = t('email.success_sent')        
+      #     format.html { redirect_to root_url }
+      #     format.json
+      #   else
+      #     flash.now[:alert] = t('email.invalidate_recipient_email')        
+      #     format.html { render :new }
+      #     format.json
+      #   end
+      # elsif params[:commit] == 'Save as draft'
+      #   @draft = Draft.new(draft_params)
+      #   if @draft.save
+      #     flash[:success] = t('email.success_saved_as_draft')
+      #     format.html { redirect_to root_url }
+      #     format.json          
+      #   else
+      #     flash.now[:alert] = t('email.fail_sent')
+      #     format.html { render :new }
+      #     format.json
+      #   end
+          format.html { redirect_to root_url }
+          format.json  
+
+      
+      # end
+    end
   end
 
-  def create_draft
-    @draft = Draft.new(params[:draft])
-    if @draft.save
-      flash[:success] = t('email.success_saved_as_draft')
-      redirect_to action: 'index'
-    else
-      flash.now[:alert] = t('email.fail_sent')
-      render :new
-    end
+  def is_a_valid_email(email)
+    email_regex = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
+    (email =~ email_regex)
+  end
+
+  private
+  def draft_params
+    params.require(:draft).permit(:to, :subject, :body, :file)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Basemail::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#new'
-
+  resources 'home'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
